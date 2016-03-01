ccb67eb642d5b3c141971e75c13317f61cb75e5c
ue-lee <ueli.leber@gmail.com>
2016-03-01 11:59:46 +0100

    changed platform_local.h on robo and frdm

---
+++ FRDM/Sources/Platform_Local.h
@@ -0,0 +1,7 @@
+#ifndef __PL_LOCAL_H_
+#define __PL_LOCAL_H_
+
+/* board identification: */
+#define PL_LOCAL_CONFIG_BOARD_IS_FRDM  (1) /* I'm the robot board */
+
+#endif /* __LOCAL_H_ */
---
+++ Robo/Sources/Platform_Local.h
@@ -0,0 +1,7 @@
+#ifndef __PL_LOCAL_H_
+#define __PL_LOCAL_H_
+
+/* board identification: */
+#define PL_LOCAL_CONFIG_BOARD_IS_ROBO  (1) /* I'm the robot board */
+
+#endif /* __PL_LOCAL_H_ */
