fc31f1a351c70c31705be8827c7c4f970f81ae94
ue-lee <ueli.leber@gmail.com>
2016-04-11 09:02:23 +0200

    changed includes

--- FRDM/Sources/Platform_Local.h
+++ FRDM/Sources/Platform_Local.h
@@ -8,7 +8,7 @@
 
 /* board identification: */
 #define PL_LOCAL_CONFIG_BOARD_IS_FRDM     (1) /* I'm the FRDM board */
-#define PL_LOCAL_CONFIG_NOF_LEDS		  (3) /* number of LEDs */
+#define PL_LOCAL_CONFIG_NOF_LEDS 		  (3) /* number of LEDs */
 #define PL_LOCAL_CONFIG_NOF_KEYS          (7) /* number of keys */
 #define PL_LOCAL_CONFIG_KEY_1_ISR         (1) /* if SW1 is using interrupts */
 #define PL_LOCAL_CONFIG_KEY_2_ISR         (1) /* if SW1 is using interrupts */
